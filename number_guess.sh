7edf0a566abd40c0a6e1621a921a6970b76e8acf
Lexiana <schaller.lexi@gmail.com>
2024-07-31 13:09:01 +0000

    feat: handle username

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo -e "\n~~~~~~ Number Guessing Game ~~~~~~\n"
+echo -e "\nEnter your username"
+read USERNAME
+
+#get user_id
+USER_ID=$($PSQL "select user_id from users where username='$USERNAME'")
+if [[ -z $USER_ID ]]
+then
+  INSERT_USER_RESULT=$($PSQL "insert into users(username) values ('$USERNAME')")
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+else
+  USER_INFO=$($PSQL "select games_played, best_game from users where user_id=$USER_ID ")
+  echo "$USER_INFO"| while IFS="|" read GAMES_PLAYED BEST_GAME
+  do
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
