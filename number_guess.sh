c7b4056fad983cb5df96ef9d07f3d5c1bbf4568b
Lexiana <schaller.lexi@gmail.com>
2024-07-31 13:31:14 +0000

    feat: number guessing game creation

--- number_guess.sh
+++ number_guess.sh
@@ -18,3 +18,33 @@ else
     echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
 fi
+
+# GAME
+GAME_WON=false
+GUESS_COUNT=0
+  # random number between 1 and 1000
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+echo -e "\nGuess the secret number between 1 and 1000:"
+
+while [[ $GAME_WON == false ]]
+do 
+  read GUESS
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo -e "\nThat is not an integer, guess again:"
+  else
+    (( GUESS_COUNT++ ))
+    if (( GUESS > SECRET_NUMBER ))
+    then
+      echo -e "\nIt's lower than that, guess again:"
+    elif (( GUESS < SECRET_NUMBER ))
+    then
+      echo -e "\nIt's higher than that, guess again:"
+    else
+      GAME_WON=true
+    fi
+  fi
+done
+
+  echo -e "\nYou guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+
